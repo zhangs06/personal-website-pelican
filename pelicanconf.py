5ef962a53747470c7b32130509f7357e24dcf41d
Ujaval Gandhi <ujaval@gmail.com>
2015-07-01 12:12:57 +0530

    add cname and static dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -72,6 +72,8 @@ SOCIAL = (('twitter', 'http://twitter.com/spatialthoughts'),
 
 DEFAULT_PAGINATION = False
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
